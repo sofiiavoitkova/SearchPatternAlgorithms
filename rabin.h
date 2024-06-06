cd4ceaedc34b5125357aefd8f76407a3f7cf97a3
Sofiia <sofiiavoitkova@gmail.com>
2024-06-06 22:46:30 +0200

    doc: add rabin.h

---
+++ rabin.h
@@ -0,0 +1,63 @@
+#ifndef RABIN_H
+#define RABIN_H
+
+#include <iostream>
+
+void rabinKarp(const std::string& P, const std::string& T) {
+    unsigned int n = T.length();
+    unsigned int m = P.length();
+
+    if (m > n) {
+        std::cout << "Pattern length is greater than text length." << std::endl;
+        return;
+    }
+
+    int S = 128;
+    int L = 997;
+
+    int hP = 0;
+    int h = 0;
+    int Sm1 = 1;
+    int count = 0;
+
+    for (int j = 1; j < m; j++) {
+        Sm1 = (Sm1 * S) % L;
+    }
+
+    for (int j = 0; j < m; j++) {
+        hP = ((hP * S) + P[j]) % L;
+        h = ((h * S) + T[j]) % L;
+    }
+
+    if (h == hP) {
+        int j = 0;
+        while (j < m && T[j] == P[j]) {
+            j++;
+        }
+        if (j == m) {
+            std::cout << "Pattern found at position 0" << std::endl;
+        }
+    }
+
+    for (int i = 0; i <= n - m; i++) {
+        if (h == hP) {
+            int j = 0;
+            while (j < m && T[j + i] == P[j]) {
+                j++;
+            }
+            if (j == m) {
+                //std::cout << "Pattern found at position " << i << std::endl;
+                count++;
+            }
+        }
+
+        if (i < n - m) {
+            h = ((h - T[i] * Sm1) * S + T[i + m]) % L;
+            if (h < 0) {
+                h += L;
+            }
+        }
+    }
+}
+
+#endif // RABIN_H
